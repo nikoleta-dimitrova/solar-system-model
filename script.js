1c380cbeac4dc5f13339e26006b7e4d185b39565
Nikoleta Dimitrova <n.dimitrova@student.fontys.nl>
2022-11-09 23:37:29 +0100

    Planet info grid + Dynamically load planet containers

--- script.js
+++ script.js
@@ -1,8 +1,113 @@
+class Planet {
+
+    constructor(id, name, size, info, image) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.info = info;
+        this.image = image
+    }
+
+    setName(name) {
+        this.name = name
+    }
+    getName() {
+        return this.name;
+    }
+
+    setSize(size) {
+        this.size = size
+    }
+    getSize() {
+        return this.size;
+    }
+
+    setInfo(info) {
+        this.info = info
+    }
+    getInfo() {
+       return this.info;
+    }
+
+    setImage(image) {
+        this.image = image
+    }
+    getImage() {
+        return this.image;
+    }
+}
+
+const loadPlanets = (callback) => {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', './json/planets.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+const init = () => {
+    loadPlanets(function (response) {
+        var planets = JSON.parse(response);
+        planets.forEach(planet => {
+            var newPlanet = new Planet(planet.id, planet.name, planet.size, planet.info, planet.image)
+            var container = createPlanetCard(newPlanet);
+            document.querySelector(".planets-container").appendChild(container);
+        })
+    })
+}
+
+const createPlanetCard = (planet) => {
+    const container = document.createElement("div");
+    container.classList.add('planet-card');
+    const containerImage = document.createElement("img");
+    containerImage.src = planet.image;
+    const containerName = document.createElement("span");
+    containerName.textContent = planet.name;
+
+    const planetDetails = document.createElement("div");
+    planetDetails.classList.add('planet-details');
+
+    const planetSize = document.createElement("div");
+    planetSize.classList.add('planet-size');
+    const sizeImg = document.createElement("img");
+    sizeImg.src="./assets/images/vector-svg-graphic-11-512.webp";
+    const planetSizeValue = document.createElement("span");
+    planetSizeValue.textContent = planet.size;
+    planetSize.appendChild(sizeImg)
+    planetSize.appendChild(planetSizeValue);
+
+
+    const planetSpeed = document.createElement("div");
+    planetSpeed.classList.add('planet-speed');
+    const speedImg = document.createElement("img");
+    speedImg.src="./assets/images/speedometer.svg";
+    const planetSpeedValue = document.createElement("span");
+    planetSpeedValue.textContent = planet.size;
+    planetSpeed.appendChild(speedImg)
+    planetSpeed.appendChild(planetSpeedValue);
+
+
+    planetDetails.appendChild(planetSize);
+    planetDetails.appendChild(planetSpeed);
+
+    container.appendChild(containerImage);
+    container.appendChild(containerName);
+    container.appendChild(planetDetails);
+
+    return container;
+}
+
+window.onload = init();
+
 var solarSystem = document.getElementById("solar-system");
 var orbits = document.querySelectorAll(".orbit");
 var sun = document.getElementById("sun");
 
-let zoomLevels = [0.15, 0.2, 0.4, 0.6, 0.8, 1];
+let zoomLevels = [0.08, 0.1, 0.15, 0.2, 0.4, 0.6, 0.8, 1];
 let currentZoom = 3;
 var _startX = 0;
 var _startY = 0;
@@ -43,16 +148,18 @@ const OnMouseMove = (event) => {
 const mouseUp = () => {
     solarSystem.onmousemove = null;
     _offsetXOrbits = [];
-    _offsetYOrbits = []
+    _offsetYOrbits = [];
 }
 
 solarSystem.addEventListener('wheel', (e) => {
-    if (e.deltaY < 0) {
-        zoomIn()
-    }
-    else if (e.deltaY > 0) {
-        zoomOut()
-    }
+    setTimeout(() => {
+        if (e.deltaY < 0) {
+            zoomIn()
+        }
+        else if (e.deltaY > 0) {
+            zoomOut()
+        }
+    }, 200);
 })
 
 solarSystem.addEventListener('mousedown', (event) => {
